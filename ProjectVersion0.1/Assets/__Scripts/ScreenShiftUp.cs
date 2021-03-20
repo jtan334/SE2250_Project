8ce3f00ef2e476e6d66a727e760befbdc2458a16
ncutler3 <ncutler3@uwo.ca>
2021-03-20 18:39:55 -0400

    made new test level and imported new test tileset
    a bunch of tests, working on scene switching and camera moving between screens

---
+++ ProjectVersion0.1/Assets/__Scripts/ScreenShiftUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class ScreenShiftUp : MonoBehaviour
+{
+    public GameObject ViewCamera;
+    private Vector3 currentPos;
+    private Vector3 originalPos;
+
+    void Awake()
+    {
+        originalPos = ViewCamera.transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentPos = ViewCamera.transform.position;
+        if (collision.gameObject.tag == "Player")
+        {
+            if (currentPos == originalPos)
+            {
+                ViewCamera.transform.position = currentPos + new Vector3(0, +20, 0);
+            }
+        }
+    }
+}
