65491734ea6c0e6b9e27467b908e8412746f6f94
ncutler3 <ncutler3@uwo.ca>
2021-03-22 15:28:15 -0400

    Optimized ScreenShift Scripts
    Combined all 4 screenShifts into one mega script, added more screen for future use. Also fixed screenShift script to work for all screens, not just ones adjacent to the original one

---
+++ ProjectVersion0.1/Assets/__Scripts/ScreenShift.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShift : MonoBehaviour
+{
+    public GameObject ViewCamera;
+    private Vector3 currentPos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        currentPos = ViewCamera.transform.position;
+
+        if (collision.gameObject.tag == "Player")
+        {
+            //up
+            if (this.transform.position.y - currentPos.y > 10)
+            {
+                ViewCamera.transform.position = currentPos + new Vector3(0, +20, 0);
+            }
+            //down
+            if (currentPos.y - this.transform.position.y > 10)
+            {
+                ViewCamera.transform.position = currentPos + new Vector3(0, -20, 0);
+            }
+            //left
+            if (currentPos.x - this.transform.position.x > 20)
+            {
+                ViewCamera.transform.position = currentPos + new Vector3(-40, 0, 0);
+            }
+            //right
+            if (this.transform.position.x - currentPos.x > 20)
+            {
+                ViewCamera.transform.position = currentPos + new Vector3(40, 0, 0);
+            }
+        }
+    }
+}
