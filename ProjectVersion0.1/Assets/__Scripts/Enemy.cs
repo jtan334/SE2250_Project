237f8003f33a0361bb214dc39261cb46868774f5
jtan334 <60947320+jtan334@users.noreply.github.com>
2021-03-23 17:27:13 -0400

    added enemy movements

--- ProjectVersion0.1/Assets/__Scripts/Enemy.cs
+++ ProjectVersion0.1/Assets/__Scripts/Enemy.cs
@@ -7,8 +7,11 @@ public class Enemy : MonoBehaviour
     public Animator animator;
     public int maxHealth = 100;
     int currentHealth;
+     private Vector3 MovingDirection = Vector3.left*2;
+   
 
     // Start is called before the first frame update
+    
     void Start()
     {
         currentHealth = maxHealth;
@@ -40,4 +43,22 @@ public class Enemy : MonoBehaviour
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
     }
+    void Update () {    
+         UpdateMovement ();
+         animator.SetTrigger("Run");
+     }
+ 
+     void UpdateMovement(){
+         if (this.transform.position.x > 7.5f) {
+             MovingDirection = Vector3.left;
+             gameObject.GetComponent<SpriteRenderer> ().flipX = false;
+ 
+         } else if (this.transform.position.x < 4.5f) { 
+             MovingDirection = Vector3.right;
+             gameObject.GetComponent<SpriteRenderer> ().flipX = true;
+ 
+         } 
+         this.transform.Translate (MovingDirection * Time.smoothDeltaTime);
+         
+     }
 }
