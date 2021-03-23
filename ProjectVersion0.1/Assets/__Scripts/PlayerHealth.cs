d0e78ac4368aa527edf97aca027a00675e157b4d
suddin25 <55818476+suddin25@users.noreply.github.com>
2021-03-23 18:14:31 -0400

    Added collision damage to BCB

---
+++ ProjectVersion0.1/Assets/__Scripts/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Animator animator;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+
+    // Takes damage after colliding with enemies
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit");
+
+            currentHealth = currentHealth - 25;
+
+            animator.SetTrigger("Hurt");
+
+            if (currentHealth < 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
