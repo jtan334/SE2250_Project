12959ba98edfffeea1ab2ee279a4ec2f071c441a
ncutler3 <ncutler3@uwo.ca>
2021-03-23 22:03:35 -0400

    MADE ALL LEVELS(SORTA COOL)
    PogChamp

--- ProjectVersion0.1/Assets/Teleport.cs
+++ ProjectVersion0.1/Assets/Teleport.cs
@@ -6,26 +6,19 @@ public class Teleport : MonoBehaviour
 {
 
     private GameObject _teleportThis;
-    private GameObject _teleportOther;
+    public GameObject teleportOther;
 
     public bool leftOrRightA;
-    public bool leftOrRightB;
     void Start()
     {
         _teleportThis = gameObject;
-        _teleportOther = gameObject.transform.parent.gameObject.transform.GetChild(0).gameObject;
-        if (_teleportOther == _teleportThis)
-        {
-            _teleportOther = gameObject.transform.parent.gameObject.transform.GetChild(1).gameObject;
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print(gameObject.name);
-        if (collision.gameObject.tag == "Player" && collision.gameObject == _teleportThis)
+        if (collision.gameObject.tag == "Player")
         {
-            collision.attachedRigidbody.transform.position = _teleportOther.transform.position;
+            collision.attachedRigidbody.transform.position = teleportOther.transform.position;
             if (leftOrRightA)
             {
                 GoRight();
@@ -35,25 +28,15 @@ public class Teleport : MonoBehaviour
                 GoLeft();
             }
         }
-        if (collision.gameObject.tag == "Player" && collision.gameObject == _teleportOther)
-        {
-            collision.attachedRigidbody.transform.position = _teleportThis.transform.position;
-            if (leftOrRightB)
-            {
-                GoRight();
-            }
-            else
-            {
-                GoLeft();
-            }
-        }
+
+
         void GoLeft()
         {
-            collision.attachedRigidbody.velocity = new Vector2(collision.attachedRigidbody.velocity.x - 40, collision.attachedRigidbody.velocity.y);
+            collision.attachedRigidbody.velocity = new Vector2(collision.attachedRigidbody.velocity.x - 50, collision.attachedRigidbody.velocity.y);
         }
         void GoRight()
         {
-            collision.attachedRigidbody.velocity = new Vector2(collision.attachedRigidbody.velocity.x + 40, collision.attachedRigidbody.velocity.y);
+            collision.attachedRigidbody.velocity = new Vector2(collision.attachedRigidbody.velocity.x + 50, collision.attachedRigidbody.velocity.y);
         }
     }
 
